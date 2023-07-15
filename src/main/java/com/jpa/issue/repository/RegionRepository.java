9d42678f01a6ed16b580ca55fd7c4ea09c0b9d75
apptie <appti@naver.com>
2023-07-15 23:35:00 +0900

    commit

--- src/main/java/com/jpa/issue/repository/RegionRepository.java
+++ src/main/java/com/jpa/issue/repository/RegionRepository.java
@@ -2,11 +2,7 @@ package com.jpa.issue.repository;
 
 import com.jpa.issue.entity.Region;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface RegionRepository extends JpaRepository<Region, Long> {
 
-    @Query("select r from Region r join fetch r.secondRegions, r.thirdRegions where r.id = :id")
-    Region findRegionWithSecondAndThirdRegion(@Param("id") Long id);
 }
